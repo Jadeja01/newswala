17af484632b9923e08623b6fe1e5c9b4bef1c9c6
Jaddu007 <you@example.com>
2024-09-28 19:29:40 +0530

    Sharing navbar through layout.js

--- app/(components)/science/page.js
+++ app/(components)/science/page.js
@@ -1,11 +1,9 @@
 import Bus_news from "@/comp_code/news_sec/business.js";
 import "bootstrap/dist/css/bootstrap.css";
 
-import Navbar from "../navbar/page";
 export default function Business() {
   return (
     <div>
-      <Navbar />
       <Bus_news category="science"/>
     </div>
   );
